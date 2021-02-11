c0ab305f1501e828665d54181acf06d819079f3a
뤗 <50108258+0xb917@users.noreply.github.com>
2021-02-11 22:37:49 +0800

    v210211

--- README.md
+++ README.md
@@ -2,29 +2,54 @@
 
 ## 设计说明
 
-我写它就是用来自动更新 NaiveProxy。
-后来我想了想，不如干脆把功能写完整发上来吧？于是就有了 naive.sh。
+我写它就是用来自动更新 Naiveproxy。
+后来我想了想，不如干脆把功能写完整发上来吧？然后就有了 naive.sh。
 它不会像[某脚本](https://github.com/233boy/v2ray)一样搞墙外墙，且不会保存在本地以避免产生垃圾 & 确保你每次使用都是最新版本。
 
 ## 要求
 
-Linux, x86 或 x86_64
+Linux, x86 / x86_64 / arm / arm64 (arm/arm64 未测试)
 以 root 运行
-已安装 wget 和 libnss3
-没了。它只做它该做的事，依赖什么的暂时就请自己解决吧。
+已安装 wget
+Debian / Ubuntu / Arch 会自动安装依赖，其他发行版请手动安装或提交 Pull Request。
+
+## 功能
+
+安装，更新和卸载 naiveproxy 客户端 / 服务端
+编辑配置文件请用 nano / vim / emacs
+管理请用 systemctl
+自动更新请用 crontab 定期执行此脚本（由于服务端为 caddy，目前没有检测版本更新的合适方式；所以 update server 指令不会对比版本，每次都会重新进行编译）
 
 ## 如何使用
 
-``` bash
-bash <(wget -qO- https://git.io/naive.sh) #正常模式
-bash <(wget -qO- https://git.io/naive.sh) update #自动更新naiveproxy
+> 注意：参数可能随更新而改变；v210211 版本未经测试，如有 bug 请提交 issue
+
+```bash
+bash <(wget -qO- https://git.io/naive.sh) #面板模式
+bash <(wget -qO- https://git.io/naive.sh) update client #自动更新客户端, 旧版的仅 update 依然可用
 ```
+
+你可以单命令安装服务端：
+
+```bash
+bash <(wget -qO- https://git.io/naive.sh) example.com auto # 使用自动生成的邮箱, user, pass, probe_resistance 进行配置
+bash <(wget -qO- https://git.io/naive.sh) example.com me@example.com username password bing.com bing.com h3 # 使用输入的邮箱, user, pass 进行配置，伪装并反代 bing.com, 开启 HTTP3
+```
+
+详细说明：
+
+- $1: 域名，需要以 A 或 AAAA 记录绑定到主机 IP。
+- $2: 邮箱或 auto, 若为 auto 则自动生成配置。
+- $3: 用户名, 当 $2 为 auto 时功能等同 $7。
+- $4: 密码
+- $5: 伪装域名
+- $6: 反向代理. 不为空或 false 即启用。
+- $7: HTTP3，不为空即启用。
 
 ## 待做
 
-- 快速安装 & 自动配置 Caddy v2 + forwardproxy
-- 支持 linux-arm, linux-arm64
-- 包管理器为 pacman, apt, yum 其一时自动安装依赖
+- 下载二进制 caddy 文件避免编译过程
+- 通过 Docker 安装服务端
 
 ## 灵感
 
--- naive.sh
+++ naive.sh
@@ -6,36 +6,52 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-if [ $(uname -m)='x86_64' ];then
-    arch="x64"
-else
-    arch="x86"
-fi
+distro=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 
-while :
-do
-    github_version=$(wget --no-check-certificate -qO- https://api.github.com/repos/klzgrad/naiveproxy/releases/latest | grep 'tag_name' | cut -d\" -f4 | sed -e "s/^v//" -e "s/-.$//")
-    if [ -n "${github_version}" ]; then
-        break
+case $(uname -m) in
+x86_64)
+    arch="x64";;
+i*86)
+    arch="x86";;
+aarch64 | aarch64_be | armv8b | armv8l)
+    arch="arm64";;
+arm | armv7l | armv6l)
+    arch="arm";;
+*)
+    arch="unknown";;
+esac
+
+check_naive_client(){
+    while :
+        do
+            github_version=$(wget --no-check-certificate -qO- https://api.github.com/repos/klzgrad/naiveproxy/releases/latest | grep 'tag_name' | cut -d\" -f4 | sed -e "s/^v//" -e "s/-.$//")
+        if [ -n "${github_version}" ]; then
+            break
+        fi
+    done
+    if [ "$(command -v /usr/local/bin/naive)" ]; then
+        local_version=$(/usr/local/bin/naive --version | sed -e "s/^naive //")
+    else
+        local_version="未安装"
     fi
-done
-if [ "$(command -v /usr/local/bin/naive)" ]; then
-    local_version=$(/usr/local/bin/naive --version | sed -e "s/^naive //")
-else
-    local_version="未安装"
-fi
-
-install_naive_after(){
-    wget --no-check-certificate -qO /etc/systemd/system/naive.service https://raw.githubusercontent.com/kwaa/naive.sh/master/naive.service
-    mkdir /etc/naive
-    read -p "设置监听端口: " port
-    echo '{"listen": "http://127.0.0.1:'${port}'","padding": true}' >> /etc/naive/config.json
-    systemctl daemon-reload
-    systemctl enable naive
-    systemctl start naive
 }
 
-install_naive(){
+install_naive_client_before(){
+    if [ "$(ldconfig -p | grep libnss3)" == "" ]; then
+    case "$distro" in
+        debian | ubuntu)
+            apt update
+            apt install libnss3;;
+        arch)
+            pacman -S nss;;
+        *)
+            echo "暂未支持你使用的发行版，请考虑提交 Pull Request 并自行安装依赖：libnss3"
+            exit 0;;
+    esac
+    fi
+}
+
+install_naive_client(){
     cd /tmp
     wget https://github.com/klzgrad/naiveproxy/releases/download/v${github_version}-1/naiveproxy-v${github_version}-1-linux-${arch}.tar.xz
     tar -xf naiveproxy-v${github_version}-1-linux-${arch}.tar.xz
@@ -47,27 +63,155 @@ install_naive(){
     rm -rf /tmp/naiveproxy-v${github_version}-1-linux-${arch}/
 }
 
-update_naive(){
+install_naive_client_after(){
+    wget -qO /etc/systemd/system/naive.service https://cdn.jsdelivr.net/gh/kwaa/naive.sh@main/naive.service
+    mkdir /etc/naive
+    while true; do
+        read -p "是否要进行配置? [Y/n]" yn
+        case $yn in
+            [Nn]*) ;;
+            *)
+                stty erase '^H'
+                read -p "设置本地监听 (socks://127.0.0.1:1080): " listen
+                read -p "设置远程地址 (https://user:pass@example.com): " proxy
+                echo '{"listen": "'${listen}'","proxy": '${proxy}'}' >> /etc/naive/config.json;;
+        esac
+    done
+    systemctl daemon-reload
+    systemctl enable naive
+    systemctl start naive
+}
+
+update_naive_client(){
     if [ "${github_version}" == "${local_version}" ]; then
         echo "本地版本与 GitHub 相同，无需更新。"
     else
         systemctl stop naive
         rm -f /usr/local/bin/naive
-        install_naive
+        install_naive_client
         systemctl start naive
     fi
 }
 
-uninstall_naive(){
-    systemctl disable naive
+uninstall_naive_client(){
     systemctl stop naive
+    systemctl disable naive
     rm -f /usr/local/bin/naive
     rm -f /etc/systemd/system/naive.service
     rm -rf /etc/naive/
     systemctl daemon-reload
 }
 
-[[ "$1" = "update" ]] && update_naive && exit 0
+install_naive_server_before(){
+    if ! [ "$(command -v go)" ]; then
+    case "$distro" in
+        debian)
+            codename=$(grep -oP '(?<=^VERSION_CODENAME=).+' /etc/os-release | tr -d '"')
+            echo 'deb http://ftp.debian.org/debian '${codename}'-backports main' | tee /etc/apt/sources.list.d/backports.list
+            apt update
+            apt -t ${codename}-backports install golang-go;;
+        ubuntu)
+            add-apt-repository ppa:longsleep/golang-backports
+            apt update
+            apt install golang-go;;
+        arch)
+            pacman -S go;;
+        *)
+            echo "暂未支持你使用的发行版，请考虑提交 Pull Request 并自行安装依赖：golang-go"
+            exit 0;;
+    esac
+    fi
+}
+
+install_naive_server(){
+    cd /tmp
+    go get -u github.com/caddyserver/xcaddy/cmd/xcaddy
+    ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
+    cp caddy /usr/bin/
+    setcap cap_net_bind_service=+ep /usr/bin/caddy
+    cd ~
+    rm -f /tmp/caddy
+}
+
+install_naive_server_after(){
+    wget -qO /etc/systemd/system/caddy.service https://cdn.jsdelivr.net/gh/caddyserver/dist@master/init/caddy.service
+    mkdir /etc/caddy
+    touch /etc/caddy/Caddyfile
+    if [ "$1" != "" ]; then
+        if [ "$2" == "auto" ]; then
+            if [ "$3" != "" ]; then echo -e '{\n  experimental_http3\n}' >> /etc/caddy/Caddyfile; fi
+            echo -e ':443, '$1'\ntls tls@'$1'\nroute {\n  forward_proxy {\n    basicauth '$(cat /proc/sys/kernel/random/uuid)' '$(cat /proc/sys/kernel/random/uuid)'\n    hide_ip\n    hide_via\n    probe_resistance '$(cat /proc/sys/kernel/random/uuid)'.com\n  }\n}' >> /etc/caddy/Caddyfile
+            sysctl -w net.ipv4.tcp_slow_start_after_idle=0
+            sysctl -w net.ipv4.tcp_notsent_lowat=16384
+        else
+            if [ "$7" != "" ]; then echo -e '{\n  experimental_http3\n}' >> /etc/caddy/Caddyfile; fi
+            echo -e ':443, '$1'\ntls '$2'\nroute {\n  forward_proxy {\n    basicauth '$3' '$4'\n    hide_ip\n    hide_via\n    probe_resistance '$5'\n  }' >> /etc/caddy/Caddyfile
+            if [[ "$6" != "" || "false" ]]; then echo -e '  reverse_proxy '$6 >> /etc/caddy/Caddyfile; fi
+            echo '}' >> /etc/caddy/Caddyfile
+        fi
+    else
+        while true; do
+        read -p "是否要进行配置? [Y/n]" yn
+        case $yn in
+            [Nn]*) ;;
+            *)
+                stty erase '^H'
+                read -p "设置域名 (example.com): " domain
+                read -p "设置邮箱 (me@example.com): " email
+                read -p "设置用户名称 (user, 为空则自动生成): " user
+                if [ "$user" == "" ]; then user=$(cat /proc/sys/kernel/random/uuid); fi
+                read -p "设置用户密码 (pass, 为空则自动生成): " pass
+                if [ "$pass" == "" ]; then pass=$(cat /proc/sys/kernel/random/uuid); fi
+                read -p "设置伪装域名 (probe_resistance, 为空则自动生成): " probe_resistance
+                read -p "设置反向代理 (reverse_proxy, 为空则无效): " reverse_proxy
+                read -p "是否要开启 HTTP/3? [y/N]:" experimental_http3
+                if [[ "$experimental_http3" == "y" || "Y" ]]; then echo -e '{\n  experimental_http3\n}\n' >> /etc/caddy/Caddyfile; fi
+                echo -e ':443, '$domain'\ntls '$email'\nroute {\n  forward_proxy {\n    basicauth '$user' '$pass'\n    hide_ip\n    hide_via\n    probe_resistance '$probe_resistance'\n  }\n' >> /etc/caddy/Caddyfile
+                if [ "$reverse_proxy" != "" ]; then echo -e '  reverse_proxy '$reverse_proxy'\n' >> /etc/caddy/Caddyfile; fi
+                echo '}' >> /etc/caddy/Caddyfile;;
+        esac
+    done
+    fi
+    systemctl daemon-reload
+    systemctl enable naive
+    systemctl start naive
+    result_userpass=$(grep -oP '(?<=basicauth )[^"]*' /etc/caddy/Caddyfile | sed 's/ /:/g')
+    result_domain=$(grep -oP '(?<=:443, )[^"]*' /etc/caddy/Caddyfile)
+    clear
+    echo '安装完成！你的代理地址是: '
+    grep -q experimental_http3 /etc/caddy/Caddyfile && echo 'quic://'$result_userpass'@'$result_domain || echo 'https://'$result_userpass'@'$result_domain
+}
+
+update_naive_server() {
+    systemctl stop caddy
+    rm -f /usr/bin/caddy
+    install_naive_server
+    systemctl start caddy
+}
+
+uninstall_naive_server() {
+    systemctl stop caddy
+    systemctl disable caddy
+    rm -f /usr/bin/caddy
+    rm -f /etc/systemd/system/caddy.service
+    rm -rf /etc/caddy/
+    systemctl daemon-reload
+}
+
+case "$1" in
+    u | update)
+        if [[ "$2" == "s" || "server" ]]; then
+            update_naive_server
+        else
+            check_naive_client && update_naive_client
+        fi
+        exit 0;;
+    *)
+        if [ "$1" != "" ]; then
+            function_test "$1" "$2" "$3" "$4" "$5" "$6"
+            exit 0
+        fi;;
+esac
 
 do_option() {
     case "$1" in
@@ -75,45 +219,65 @@ do_option() {
             exit 0
             ;;
         1)
-            install_naive
-            install_naive_after
+            install_naive_client_before
+            install_naive_client
+            install_naive_client_after
             exit 0
             ;;
         2)
-            uninstall_naive
+            uninstall_naive_client
             exit 0
             ;;
         3)
-            update_naive
+            check_naive_client
+            update_naive_client
             exit 0
             ;;
         4)
-            port_test
+            install_naive_server_before
+            install_naive_server
+            install_naive_server_after
             exit 0
+            ;;
+        5)
+            uninstall_naive_server
+            exit 0
+            ;;
+        6)
+            update_naive_server
+            exit 0
+            ;;
+        *)
             ;;
     esac
 }
+
+check_naive_client
 
 while :
 do
     clear
     echo "/* ----------------------------------------------------*"
-    echo "* naive.sh  v0.200912                                  *"
+    echo "* naive.sh  v0.210211                                  *"
     echo "* created by kwaa                                      *"
     echo "* intro: https://kwaa.dev/p/naive-sh                   *"
     echo "* source: https://github.com/kwaa/naive.sh             *"
     echo "* --------------------------------------------------- */"
-    echo "最新版本:" ${github_version}
-    echo "本地版本:" ${local_version}
+    echo "最新客户端版本:" ${github_version}
+    echo "本地客户端版本:" ${local_version}
+    echo "系统类型:" ${distro} ${arch}
 while :
 do
-    echo "0) 退出脚本"
-    echo "1) 安装 NaiveProxy"
-    echo "2) 卸载 NaiveProxy"
-    echo "3) 更新 Naiveproxy"
+    echo "0) 退出"
+    echo "1) 安装客户端"
+    echo "2) 卸载客户端"
+    echo "3) 更新客户端"
+    echo "4) 安装服务端"
+    echo "5) 卸载服务端"
+    echo "6) 更新服务端"
     unset choose_an_option
     read -p "输入一个数字: " choose_an_option
-    if [[ "$choose_an_option" = "0" ]] || [[ "$choose_an_option" = "1" ]] || [[ "$choose_an_option" = "2" ]] || [[ "$choose_an_option" = "3" ]]; then
+    if echo $choose_an_option | grep -qE '^[0-9]+$'; then
         do_option $choose_an_option
         break
     else
